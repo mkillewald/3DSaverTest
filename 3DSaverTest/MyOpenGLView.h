c066d209a161502296ddc91fa2d2180b9fef71ac
Mike Killewald <mkillewald@yahoo.com>
2017-07-10 16:38:28 -0400

    filename clean up

---
+++ 3DSaverTest/MyOpenGLView.h
@@ -0,0 +1,15 @@
+//
+//  MyOpenGLView.h
+//  3DSaverTest
+//
+//  Created by k1ds3ns4t10n on 4/12/17.
+//  Copyright © 2017 Gameaholix. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Gameaholix_3DSaverTest_MyOpenGLView : NSOpenGLView
+
+@end
+
+@compatibility_alias MyOpenGLView Gameaholix_3DSaverTest_MyOpenGLView;
